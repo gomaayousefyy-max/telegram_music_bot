b63623db3175df69318122c293c9f28178971d4d
gomaayousefyy-max <gomaayousefyy@gmail.com>
2026-07-29 03:16:08 +0300

    Update music_helpers.py

--- music_helpers.py
+++ music_helpers.py
@@ -190,10 +190,8 @@ def _ydl_opts() -> dict:
     opts = {
         "format": Config.YDL_FORMAT,
         "outtmpl": os.path.join(Config.DOWNLOAD_DIR, "%(id)s.%(ext)s"),
-        "impersonate": "chrome",
         "quiet": True,
         "no_warnings": True,
-        
         "noplaylist": False,
         "nocheckcertificate": True,
         "geo_bypass": True,
